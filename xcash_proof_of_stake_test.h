dcbf7d05f059e5222b1cc8b6b8c794e7102f0f53
zachhildreth <zachariahildreth@gmail.com>
2019-03-10 14:26:10 -0700

    Added reset_variables_allocated_on_the_heap_test function
    Added reset_variables_allocated_on_the_heap_test function.
    
    This function runs each function 1000 times to check if all variables allocated on the heap are reset.
    
    On the first run, the function gets the amount of memory used for the test function. It then runs it 10 times and checks if the memory usage has gone up by 900%. if not it then runs 100 times and checks for 500% and then 1000 times and checks for 100%. These levels of checks will detect major and minor variables that are allocated on the heap and are not reset.
    
    Added get_program_memory_usage function to get the current memory usage of the program.

--- xcash_proof_of_stake_test.h
+++ xcash_proof_of_stake_test.h
@@ -19,6 +19,8 @@ void* create_server_on_separate_thread();
 int read_and_write_file_test();
 int send_and_receive_data_socket_test();
 int analysing_code_test();
+size_t get_program_memory_usage(char* process_id_file);
+int reset_variables_allocated_on_the_heap_test();
 void test();
 
 #endif
