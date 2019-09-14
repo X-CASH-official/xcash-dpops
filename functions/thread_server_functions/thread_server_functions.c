27a7411f02b59be507166ecfa987088410f67bae
zachhildreth <zachariahildreth@gmail.com>
2019-09-14 13:08:32 -0700

    Added calculate_block_reward_for_each_delegate function
    Added calculate_block_reward_for_each_delegate function.

--- functions/thread_server_functions/thread_server_functions.c
+++ functions/thread_server_functions/thread_server_functions.c
@@ -567,6 +567,313 @@ void* check_delegates_online_status_timer_thread()
 
 /*
 -----------------------------------------------------------------------------------------------------------
+Name: add_block_to_blocks_found
+Description: Adds the block to the blocks_found database
+-----------------------------------------------------------------------------------------------------------
+*/
+
+int add_block_to_blocks_found()
+{
+  // Variables
+  char data[BUFFER_SIZE];
+  char data2[BUFFER_SIZE];
+  char block_height[BUFFER_SIZE];
+  char block_hash[BUFFER_SIZE];  
+  char block_reward[BUFFER_SIZE];
+  char block_date_and_time[BUFFER_SIZE];
+  int count;
+  int count2;
+  long long int number;
+  long long int block_height_number;
+  long long int block_reward_number;
+  struct database_multiple_documents_fields database_data;
+  int document_count = 0;
+
+  // define macros
+  #define TOTAL_BLOCKS_FOUND_DATABASE_FIELDS 5
+
+  #define ADD_BLOCK_TO_BLOCKS_FOUND_ERROR(message) \
+  memcpy(error_message.function[error_message.total],"add_block_to_blocks_found",25); \
+  memcpy(error_message.data[error_message.total],message,sizeof(message)-1); \
+  error_message.total++; \
+  return 0;
+
+  #define pointer_reset_database_array \
+  for (count = 0; count < document_count; count++) \
+  { \
+    for (count2 = 0; count2 < TOTAL_BLOCKS_FOUND_DATABASE_FIELDS; count2++) \
+    { \
+      pointer_reset(database_data.item[count][count2]); \
+      pointer_reset(database_data.value[count][count2]); \
+    } \
+  }
+
+  memset(data,0,sizeof(data));
+  memset(data2,0,sizeof(data2));
+  memset(block_height,0,sizeof(block_height));
+  memset(block_hash,0,sizeof(block_hash));
+  memset(block_reward,0,sizeof(block_reward));
+  memset(block_date_and_time,0,sizeof(block_date_and_time));
+
+  // initialize the database_data struct 
+  document_count = count_all_documents_in_collection(DATABASE_NAME_DELEGATES,"blocks_found",0);
+  for (count = 0; count < document_count; count++)
+  {
+    for (count2 = 0; count2 < TOTAL_BLOCKS_FOUND_DATABASE_FIELDS; count2++)
+    {
+      database_data.item[count][count2] = (char*)calloc(100,sizeof(char));
+      database_data.value[count][count2] = (char*)calloc(100,sizeof(char));
+
+      if (database_data.item[count][count2] == NULL || database_data.value[count][count2] == NULL)
+      {
+        color_print("Could not allocate the variables on the heap","red");
+        exit(0);
+      }
+    }
+  }
+  database_data.document_count = 0;
+  database_data.database_fields_count = 0;
+
+  // get the block information of the previous block
+  if (get_previous_block_information(block_hash,block_reward,block_date_and_time) == 0)
+  {
+    ADD_BLOCK_TO_BLOCKS_FOUND_ERROR("Could not get the previous block information.\nCould not check if the block verifier found the last block.");
+  }
+
+  // convert the block_reward to a number
+  sscanf(block_reward, "%lld", &block_reward_number);
+
+  // get the block height of the previous block
+  sscanf(current_block_height, "%lld", &number);
+  number--;
+  sprintf(block_height,"%lld",number);
+
+  // get the block count
+  if (read_multiple_documents_all_fields_from_collection(DATABASE_NAME_DELEGATES,"blocks_found","",&database_data,1,document_count,1,"total_vote_count_number",0) == 0)
+  {
+    ADD_BLOCK_TO_BLOCKS_FOUND_ERROR("Could not read the blocks_found database.\nCould not check if the block verifier found the last block.");
+  }
+
+  // loop through all blocks found, and find the last block height
+  for (block_reward_number = 0, count = 0; count < document_count; count++)
+  {
+    for (count2 = 0; count2 < TOTAL_BLOCKS_FOUND_DATABASE_FIELDS; count2++)
+    {
+      if (memcmp(database_data.item[count][count2],"block_height",12) == 0)
+      {
+        sscanf(database_data.value[count][count2], "%lld", &number);
+        if (number > block_height_number)
+        {
+          block_height_number = number;
+        }
+      }
+    }
+  }
+
+  memset(data2,0,sizeof(data2));
+  // get the block height of the previous block
+  sscanf(current_block_height, "%lld", &number);
+  number--;
+  number -= block_height_number;
+  sprintf(data2,"%lld",block_height_number);
+
+  // add the blocks data
+  memset(data,0,sizeof(data));
+  memcpy(data,"{\"block_height\":\"",17);
+  memcpy(data+strlen(data),block_height,strnlen(block_height,sizeof(data)));
+  memcpy(data+strlen(data),"\",\"block_hash\":\"",16);
+  memcpy(data+strlen(data),block_hash,BLOCK_HASH_LENGTH);
+  memcpy(data+strlen(data),"\",\"block_date_and_time\":\"",25);
+  memcpy(data+strlen(data),block_date_and_time,strnlen(block_date_and_time,sizeof(data)));
+  memcpy(data+strlen(data),"\",\"block_reward\":\"",18);
+  memcpy(data+strlen(data),block_reward,strnlen(block_reward,sizeof(data)));
+  memcpy(data+strlen(data),"\",\"block_count\":\"",17);
+  memcpy(data+strlen(data),data2,strnlen(data2,sizeof(data)));
+  memcpy(data+strlen(data),"\"}",2);
+  
+  if (insert_document_into_collection_json(DATABASE_NAME_DELEGATES,"blocks_found",data,0) == 0)
+  {
+    ADD_BLOCK_TO_BLOCKS_FOUND_ERROR("Could not add the block to the blocks_found database.\nCould not check if the block verifier found the last block.");
+  }
+  pointer_reset_database_array;
+  return 1;
+
+  #undef TOTAL_BLOCKS_FOUND_DATABASE_FIELDS
+  #undef ADD_BLOCK_TO_BLOCKS_FOUND_ERROR
+}
+
+
+
+/*
+-----------------------------------------------------------------------------------------------------------
+Name: calculate_block_reward_for_each_delegate
+Description: Calculates the block reward for each delegate
+Parameters:
+  block_reward - The block reward
+Return: 0 if an error has occured, 1 if successfull
+-----------------------------------------------------------------------------------------------------------
+*/
+
+int calculate_block_reward_for_each_delegate(long long int block_reward)
+{
+  // Variables
+  char data[BUFFER_SIZE];
+  char data2[BUFFER_SIZE];
+  char data3[BUFFER_SIZE];
+  char current_delegates_public_address[BUFFER_SIZE];
+  char current_delegates_total_votes[BUFFER_SIZE];
+  int count;
+  int counter;
+  int public_address_count;
+  long long int number;
+  long long int total_votes;
+  long long int current_delegates_block_reward;
+  struct database_multiple_documents_fields database_data;
+  int document_count = 0;  
+
+  // define macros
+  #define CALCULATE_BLOCK_REWARD_FOR_EACH_DELEGATE_ERROR(message) \
+  memcpy(error_message.function[error_message.total],"calculate_block_reward_for_each_delegate",40); \
+  memcpy(error_message.data[error_message.total],message,sizeof(message)-1); \
+  error_message.total++; \
+  pointer_reset_database_array; \
+  return 0;
+
+  #define pointer_reset_database_array \
+  for (count = 0; count < public_address_count; count++) \
+  { \
+    for (counter = 0; counter < TOTAL_RESERVE_PROOFS_DATABASE_FIELDS; counter++) \
+    { \
+      pointer_reset(database_data.item[count][counter]); \
+      pointer_reset(database_data.value[count][counter]); \
+    } \
+  }
+
+  memset(data,0,sizeof(data));
+  memset(data2,0,sizeof(data2));
+  memset(data3,0,sizeof(data3));
+
+  // create the message
+  memset(data,0,strlen(data));
+  memcpy(data,"{\"public_address_voted_for\":\"",29);
+  memcpy(data+29,xcash_wallet_public_address,XCASH_WALLET_LENGTH);
+  memcpy(data+127,"\"}",2); 
+
+  // get the count of how many public addresses voted for the delegate
+  for (public_address_count = 0, count = 1; count <= TOTAL_RESERVE_PROOFS_DATABASES; count++)
+  { 
+    memset(data2,0,strlen(data2));
+    memcpy(data2,"reserve_proofs_",15);
+    snprintf(data2+15,sizeof(data2)-1,"%d",count);
+    public_address_count += count_documents_in_collection(DATABASE_NAME,data2,data,0);
+  }
+
+  // error check
+  if (public_address_count == 0)
+  {
+    CALCULATE_BLOCK_REWARD_FOR_EACH_DELEGATE_ERROR("Could not find the block verifiers public address in the reserve proofs database.\nCould not calculate the block reward for each delegate");
+  }
+
+  // initialize the database_multiple_documents_fields struct 
+  for (count = 0; count < public_address_count; count++)
+  {
+    for (counter = 0; counter < TOTAL_RESERVE_PROOFS_DATABASE_FIELDS; counter++)
+    {
+      database_data.item[count][counter] = (char*)calloc(BUFFER_SIZE_RESERVE_PROOF,sizeof(char));
+      database_data.value[count][counter] = (char*)calloc(BUFFER_SIZE_RESERVE_PROOF,sizeof(char));
+    }
+  }
+  database_data.document_count = 0;
+  database_data.database_fields_count = 0;
+
+  // get the count of how many public addresses voted for the delegate
+  for (counter = 0, count = 1; count <= TOTAL_RESERVE_PROOFS_DATABASES; count++)
+  { 
+    memset(data2,0,strlen(data2));
+    memcpy(data2,"reserve_proofs_",15);
+    snprintf(data2+15,sizeof(data2)-1,"%d",count);
+    counter = count_documents_in_collection(DATABASE_NAME,data2,data,0);
+    if (counter > 0)
+    {
+      if (read_multiple_documents_all_fields_from_collection(DATABASE_NAME,data2,data,&database_data,1,counter,0,"",0) == 0)
+      {
+        CALCULATE_BLOCK_REWARD_FOR_EACH_DELEGATE_ERROR("Could not read the reserve proofs database.\nCould not calculate the block reward for each delegate");
+      }
+    }
+  }
+
+  // get the total votes
+  for (total_votes = 0, count = 0; count < database_data.document_count; count++)
+  {
+    for (counter = 0; counter < TOTAL_RESERVE_PROOFS_DATABASE_FIELDS; counter++)
+    {
+      if (memcmp(database_data.item[count][counter],"total",5) == 0)
+      {
+        sscanf(database_data.value[count][counter], "%lld", &number);
+        total_votes += number;
+      }      
+    }
+  }
+
+  // calculate the block reward for all of the public_addresses and the fee
+  for (count = 0; count < database_data.document_count; count++)
+  {
+    // get the current delegates public address and total votes
+    for (counter = 0; counter < TOTAL_RESERVE_PROOFS_DATABASE_FIELDS; counter++)
+    {      
+      if (memcmp(database_data.item[count][counter],"public_address_created_reserve_proof",36) == 0)
+      {
+        memset(current_delegates_public_address,0,sizeof(current_delegates_public_address));
+        memcpy(current_delegates_public_address,database_data.item[count][counter],strnlen(database_data.item[count][counter],sizeof(current_delegates_public_address)));
+      }
+      else if (memcmp(database_data.item[count][counter],"total",5) == 0)
+      {
+        memset(current_delegates_total_votes,0,sizeof(current_delegates_total_votes));
+        memcpy(current_delegates_total_votes,database_data.item[count][counter],strnlen(database_data.item[count][counter],sizeof(current_delegates_total_votes)));
+      }
+    }
+
+    // get the votes
+    sscanf(current_delegates_total_votes, "%lld", &number);
+    current_delegates_block_reward = (long long int)(((double)number / (double)total_votes) * (double)block_reward) | 0;
+    
+    // create the message
+    memset(data2,0,sizeof(data2));
+    memcpy(data2,"{\"public_address\":\"",19);
+    memcpy(data2+19,current_delegates_public_address,XCASH_WALLET_LENGTH);
+    memcpy(data2+117,"\"}",2);
+    memset(data,0,sizeof(data));
+    
+    if (read_document_field_from_collection(DATABASE_NAME_DELEGATES,"public_addresses",data2,"current_total",data,0) == 0)
+    {
+      CALCULATE_BLOCK_REWARD_FOR_EACH_DELEGATE_ERROR("Could not calculate block reward for all of the public address that voted for the delegate.");
+    }
+    
+    sscanf(data, "%lld", &number);
+    number += current_delegates_block_reward;
+    memset(data,0,sizeof(data));
+    memset(data3,0,sizeof(data3));
+    sprintf(data3,"%lld",number);
+    memcpy(data2,"{\"current_total\":\"",18);
+    memcpy(data2+18,data3,strnlen(data3,sizeof(data2)));
+    memcpy(data2+strlen(data2),"\"}",2);
+    
+    if (update_document_from_collection(DATABASE_NAME_DELEGATES,"public_addresses",data2,data,0) == 0)
+    {
+      CALCULATE_BLOCK_REWARD_FOR_EACH_DELEGATE_ERROR("Could not calculate block reward for all of the public address that voted for the delegate.");
+    } 
+  }
+  pointer_reset_database_array;
+  return 1;
+
+  #undef CALCULATE_BLOCK_REWARD_FOR_EACH_DELEGATE_ERROR
+  #undef pointer_reset_database_array
+}
+
+
+
+/*
+-----------------------------------------------------------------------------------------------------------
 Name: send_data_socket_thread
 Description: Send a message through a socket on a separate thread
 Parameters:
--- functions/thread_server_functions/thread_server_functions.h
+++ functions/thread_server_functions/thread_server_functions.h
@@ -11,6 +11,8 @@ void* current_block_height_timer_thread();
 int compare(const void* STRING1, const void* STRING2);
 void* check_reserve_proofs_timer_thread();
 void* check_delegates_online_status_timer_thread();
+int add_block_to_blocks_found();
+int calculate_block_reward_for_each_delegate(long long int block_reward);
 void* send_data_socket_thread(void* parameters);
 void* send_and_receive_data_socket_thread(void* parameters);
 void* socket_receive_data_thread(void* parameters);
