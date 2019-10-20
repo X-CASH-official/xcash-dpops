9eb4b9519d85c87150cff81cdf2812211b3f16a0
zachhildreth <zachariahildreth@gmail.com>
2019-10-20 06:56:03 -0700

    General update
    General update

--- src/functions/block_verifiers_functions/block_verifiers_synchronize_functions/block_verifiers_synchronize_server_functions/block_verifiers_synchronize_server_functions.c
+++ src/functions/block_verifiers_functions/block_verifiers_synchronize_functions/block_verifiers_synchronize_server_functions/block_verifiers_synchronize_server_functions.c
@@ -174,17 +174,6 @@ int server_receive_data_socket_node_to_network_data_nodes_get_current_block_veri
   
   memset(data,0,sizeof(data));
 
-  // get the total delegates
-  total_delegates = count_all_documents_in_collection(DATABASE_NAME,DATABASE_COLLECTION,0);
-  if (total_delegates == 0)
-  {
-    SERVER_RECEIVE_DATA_SOCKET_NODE_TO_NETWORK_DATA_NODES_GET_CURRENT_BLOCK_VERIFIERS_LIST_ERROR("Could not send the NETWORK_DATA_NODE_TO_NODE_SEND_CURRENT_BLOCK_VERIFIERS_LIST message to the block verifier");
-  }
-  else if (total_delegates > BLOCK_VERIFIERS_TOTAL_AMOUNT)
-  {
-    total_delegates = BLOCK_VERIFIERS_TOTAL_AMOUNT;
-  }
-
   // create the message
   memcpy(data,"{\r\n \"message_settings\": \"NETWORK_DATA_NODE_TO_NODE_SEND_CURRENT_BLOCK_VERIFIERS_LIST\",\r\n \"block_verifiers_public_address_list\": \"",129);
   for (count2 = 0; count2 < total_delegates; count2++)
