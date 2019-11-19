be207b05513d3447d9b64cf1f2519ac8c79f16ec
zachhildreth <zachariahildreth@gmail.com>
2019-11-19 08:20:17 -0800

    Increased the maximum reserve proof size
    Increased the maximum reserve proof size, so it will accept a reserve proof up to around 100 unspents.

--- src/global_data/define_macros.h
+++ src/global_data/define_macros.h
@@ -85,7 +85,7 @@ Using define statements instead of constants for increased efficiency
 
 // Lengths
 #define BUFFER_SIZE 164000
-#define BUFFER_SIZE_RESERVE_PROOF 10000
+#define BUFFER_SIZE_RESERVE_PROOF 35000
 #define BUFFER_SIZE_NETWORK_BLOCK_DATA 500
 #define BUFFER_SIZE_NETWORK_BLOCK_TRANSACTIONS_DATA 100
 #define MAXIMUM_BUFFER_SIZE 52428800 // 50 MB
