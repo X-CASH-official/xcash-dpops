916d9286cc47f736ac0c38a11df94fcd45bd6b62
zachhildreth <zachariahildreth@gmail.com>
2019-11-21 12:21:16 -0800

    Updated build script to run non interactively for docker
    Updated build script to run non interactively for docker.

--- scripts/build/script.sh
+++ scripts/build/script.sh
@@ -4,21 +4,21 @@ rm -r /var/www/html/build/
 mkdir /var/www/html/build/
 
 docker start ubuntu-18.04
-docker exec -e USER="root" -u root -t -i --privileged ubuntu-18.04 /bin/bash -c "source ~/.profile; $(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script.sh)"
+docker exec -e USER="root" -u root --privileged ubuntu-18.04 /bin/bash -c "source ~/.profile; $(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script.sh)"
 docker cp ubuntu-18.04:/root/x-network/build/ /var/www/html/build/ubuntu-18.04
 docker stop ubuntu-18.04
 
 docker start ubuntu-19.10
-docker exec -e USER="root" -u root -t -i --privileged ubuntu-19.10 /bin/bash -c "source ~/.profile; $(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script.sh)"
+docker exec -e USER="root" -u root --privileged ubuntu-19.10 /bin/bash -c "source ~/.profile; $(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script.sh)"
 docker cp ubuntu-19.10:/root/x-network/build/ /var/www/html/build/ubuntu-19.10
 docker stop ubuntu-19.10
 
 docker start ubuntu-18.04-static-x-cash-build
-docker exec -e USER="root" -u root -t -i --privileged ubuntu-18.04-static-x-cash-build /bin/bash -c "$(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script_static.sh)"
+docker exec -e USER="root" -u root --privileged ubuntu-18.04-static-x-cash-build /bin/bash -c "$(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script_static.sh)"
 docker cp ubuntu-18.04-static-x-cash-build:/root/x-network/build/STEP_10_XCASH_BUILD_STATIC.txt /var/www/html/build/ubuntu-18.04/STEP_10_XCASH_BUILD_STATIC.txt
 docker stop ubuntu-18.04-static-x-cash-build
 
 docker start ubuntu-19.10-static-x-cash-build
-docker exec -e USER="root" -u root -t -i --privileged ubuntu-19.10-static-x-cash-build /bin/bash -c "$(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script_static.sh)"
+docker exec -e USER="root" -u root --privileged ubuntu-19.10-static-x-cash-build /bin/bash -c "$(curl -sSL https://raw.githubusercontent.com/X-CASH-official/XCASH_DPOPS/master/scripts/build/build_script_static.sh)"
 docker cp ubuntu-19.10-static-x-cash-build:/root/x-network/build/STEP_10_XCASH_BUILD_STATIC.txt /var/www/html/build/ubuntu-19.10/STEP_10_XCASH_BUILD_STATIC.txt
 docker stop ubuntu-19.10-static-x-cash-build
