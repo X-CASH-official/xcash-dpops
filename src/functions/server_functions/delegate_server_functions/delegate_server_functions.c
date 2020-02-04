abedd6d2fcda2c6ec008d8b93a16c68e71e7beb5
zachhildreth <zachariahildreth@gmail.com>
2020-02-04 11:03:22 -0800

    Added fee structure check to update delegates information
    Added better checking for an invalid fee structure when updating the delegates information.

--- src/functions/server_functions/delegate_server_functions/delegate_server_functions.c
+++ src/functions/server_functions/delegate_server_functions/delegate_server_functions.c
@@ -695,7 +695,7 @@ int server_receive_data_socket_nodes_to_block_verifiers_update_delegates(const i
   }
 
   // check if the value is valid
-  if ((memcmp(item,"IP_address",10) == 0 && strlen(value) > 255) || (memcmp(item,"about",5) == 0 && strlen(value) > 1024) || (memcmp(item,"website",7) == 0 && strlen(value) > 255) || (memcmp(item,"team",4) == 0 && strlen(value) > 255) || (memcmp(item,"pool_mode",9) == 0 && memcmp(value,"true",4) != 0 && memcmp(value,"false",5) != 0) || (memcmp(item,"fee_structure",13) == 0 && strlen(value) > 10) || (memcmp(item,"server_settings",15) == 0 && strlen(value) > 1024))
+  if ((memcmp(item,"IP_address",10) == 0 && strlen(value) > 255) || (memcmp(item,"about",5) == 0 && strlen(value) > 1024) || (memcmp(item,"website",7) == 0 && strlen(value) > 255) || (memcmp(item,"team",4) == 0 && strlen(value) > 255) || (memcmp(item,"pool_mode",9) == 0 && memcmp(value,"true",4) != 0 && memcmp(value,"false",5) != 0) || (memcmp(item,"fee_structure",13) == 0 && check_for_valid_fee_structure(value) == 0) || (memcmp(item,"server_settings",15) == 0 && strlen(value) > 1024))
   {    
     SERVER_RECEIVE_DATA_SOCKET_NODES_TO_BLOCK_VERIFIERS_UPDATE_DELEGATE_ERROR("Invalid item value to update");
   }
