9bb94e32ed5a5f65393a578a651a5d6aaafdf2dc
zachhildreth <zachariahildreth@gmail.com>
2020-02-09 03:43:53 -0800

    Updated shared delegate website test
    Updated shared delegate website test

--- src/tests/shared_delegate_website_functions_test/shared_delegate_website_functions_test.c
+++ src/tests/shared_delegate_website_functions_test/shared_delegate_website_functions_test.c
@@ -38,6 +38,9 @@ int shared_delegate_website_functions_test(void)
 
   // define macros
   #define SHARED_DELEGATE_WEBSITE_FUNCTIONS_TOTAL_TEST 10
+  #define GET_BLOCKS_FOUND_TEST_DATA_RESULTS "[{\"block_height\":\"521850\",\"block_hash\":\"0000000000000000000000000000000000000000000000000000000000000000\",\"block_date_and_time\":\"10\",\"block_reward\":\"15\"},{\"block_height\":\"440850\",\"block_hash\":\"0000000000000000000000000000000000000000000000000000000000000000\",\"block_date_and_time\":\"10\",\"block_reward\":\"10\"}]"
+  #define GET_PUBLIC_ADDRESS_INFORMATION_DATA_RESULTS "{\"public_address\":\"XCA1v18Qsf5PKLr8GFr14jHkjgf3mPm1MAVbswBs9QP7FwGTLCE4SwYi81BRp2vrcV12maMtCw9TE1NZRVyynQ3e2c3b7mxRw3\",\"current_total\":\"5\",\"total\":\"10\",\"inactivity_count\":\"15\"}"
+  #define GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION_DATA_RESULTS "[{\"public_address\":\"XCA1v18Qsf5PKLr8GFr14jHkjgf3mPm1MAVbswBs9QP7FwGTLCE4SwYi81BRp2vrcV12maMtCw9TE1NZRVyynQ3e2c3b7mxRw3\",\"date_and_time\":\"5\",\"total\":\"10\",\"tx_hash\":\"TX_HASH\",\"tx_key\":\"TX_KEY\"},{\"public_address\":\"XCA1v18Qsf5PKLr8GFr14jHkjgf3mPm1MAVbswBs9QP7FwGTLCE4SwYi81BRp2vrcV12maMtCw9TE1NZRVyynQ3e2c3b7mxRw3\",\"date_and_time\":\"5\",\"total\":\"10\",\"tx_hash\":\"TX_HASH\",\"tx_key\":\"TX_KEY\"}]"
   
   memset(database_name,0,sizeof(database_name));
   memcpy(database_name,DATABASE_NAME_TEST,sizeof(DATABASE_NAME_TEST)-1);
@@ -46,17 +49,27 @@ int shared_delegate_website_functions_test(void)
   delete_database(database_name,0);
   shared_delegates_website = 1;
   insert_document_into_collection_json(database_name,"reserve_bytes_1","{\"message_settings\": \"NODE_TO_BLOCK_VERIFIERS_GET_RESERVE_BYTES_DATABASE_HASH\",\"block_height\": \"" XCASH_PROOF_OF_STAKE_BLOCK_HEIGHT_TEST "\"}",0);
-  insert_document_into_collection_json(database_name,"reserve_proofs_1",RESERVE_PROOFS_TEST_DATA,0);
-  insert_document_into_collection_json(database_name,"delegates",DELEGATES_TEST_DATA,0);
   insert_document_into_collection_json(database_name,"statistics",DATABASE_COLLECTION_STATISTICS_DATA,0);
-  insert_document_into_collection_json(database_name,"blocks_found","{\"block_height\":\"" XCASH_PROOF_OF_STAKE_BLOCK_HEIGHT_TEST "\",\"block_hash\":\"0000000000000000000000000000000000000000000000000000000000000000\",\"block_date_and_time\":\"10\",\"block_reward\":\"15\",\"block_count\":\"10\"}",0);
+  insert_document_into_collection_json(database_name,"blocks_found","{\"block_height\":\"" XCASH_PROOF_OF_STAKE_BLOCK_HEIGHT_TEST "\",\"block_hash\":\"0000000000000000000000000000000000000000000000000000000000000000\",\"block_date_and_time\":\"10\",\"block_reward\":\"15\"}",0);
+  insert_document_into_collection_json(database_name,"blocks_found","{\"block_height\":\"440850\",\"block_hash\":\"0000000000000000000000000000000000000000000000000000000000000000\",\"block_date_and_time\":\"10\",\"block_reward\":\"10\"}",0);
   insert_document_into_collection_json(database_name,"public_addresses","{\"public_address\":\"" TEST_WALLET "\",\"current_total\":\"5\",\"total\":\"10\",\"inactivity_count\":\"15\"}",0);
+  insert_document_into_collection_json(database_name,"public_addresses","{\"public_address\":\"XCA1u5AWxhjCtRKzZiyUUmdHLnQFQdFcZGtuFxx9JW21hWJnzgf31AV2g58gUsN3aKSju1iL9RdzbHJqvUKjVYBh67e74Zc38v\",\"current_total\":\"5\",\"total\":\"10\",\"inactivity_count\":\"15\"}",0);
+  insert_document_into_collection_json(database_name,"public_addresses_payments","{\"public_address\":\"" TEST_WALLET "\",\"date_and_time\":\"5\",\"total\":\"10\",\"tx_hash\":\"TX_HASH\",\"tx_key\":\"TX_KEY\"}",0);
   insert_document_into_collection_json(database_name,"public_addresses_payments","{\"public_address\":\"" TEST_WALLET "\",\"date_and_time\":\"5\",\"total\":\"10\",\"tx_hash\":\"TX_HASH\",\"tx_key\":\"TX_KEY\"}",0);
   memset(result_test,0,sizeof(result_test));
   memcpy(result_test,"{\"public_address\":\"",19);
   memcpy(result_test+strlen(result_test),xcash_wallet_public_address,XCASH_WALLET_LENGTH);
-  memcpy(result_test+strlen(result_test),"\",\"total_vote_count\":\"0\",\"IP_address\":\"127.0.0.1\",\"delegate_name\":\"delegate_name_1\",\"about\":\"about\",\"website\":\"website\",\"team\":\"team\",\"pool_mode\":\"false\",\"fee_structure\":\"0\",\"server_settings\":\"server settings\",\"block_verifier_score\":\"0\",\"online_status\":\"true\",\"block_verifier_total_rounds\":\"0\",\"block_verifier_online_total_rounds\":\"0\",\"block_verifier_online_percentage\":\"0\",\"block_producer_total_rounds\":\"0\",\"block_producer_block_heights\":\"440875\",\"public_key\":\"ad98cadb3b13229c78709876955247cbef40d5b15c4842be605b0e8b30c97a7a\"}",528);
+  memcpy(result_test+strlen(result_test),"\",\"total_vote_count\":\"2000000000\",\"IP_address\":\"127.0.0.1\",\"delegate_name\":\"delegate_name_1\",\"about\":\"about\",\"website\":\"website\",\"team\":\"team\",\"pool_mode\":\"false\",\"fee_structure\":\"0\",\"server_settings\":\"server settings\",\"block_verifier_score\":\"0\",\"online_status\":\"true\",\"block_verifier_total_rounds\":\"1\",\"block_verifier_online_total_rounds\":\"1\",\"block_verifier_online_percentage\":\"100\",\"block_producer_total_rounds\":\"2\",\"block_producer_block_heights\":\"|440875|440876\",\"public_key\":\"ad98cadb3b13229c78709876955247cbef40d5b15c4842be605b0e8b30c97a7a\"}",547);
   insert_document_into_collection_json(database_name,"delegates",result_test,0);
+  insert_document_into_collection_json(database_name,"delegates","{\"public_address\":\"XCA1u5AWxhjCtRKzZiyUUmdHLnQFQdFcZGtuFxx9JW21hWJnzgf31AV2g58gUsN3aKSju1iL9RdzbHJqvUKjVYBh67e74Zc38v\",\"total_vote_count\":\"1000000000\",\"IP_address\":\"192.168.1.220\",\"delegate_name\":\"delegate_name_20\",\"about\":\"\",\"website\":\"\",\"team\":\"\",\"pool_mode\":\"false\",\"fee_structure\":\"\",\"server_settings\":\"\",\"block_verifier_score\":\"0\",\"online_status\":\"true\",\"block_verifier_total_rounds\":\"0\",\"block_verifier_online_total_rounds\":\"0\",\"block_verifier_online_percentage\":\"0\",\"block_producer_total_rounds\":\"0\",\"block_producer_block_heights\":\"\",\"public_key\":\"ad98cadb3b13229c78709876955247cbef40d5b15c4842be605b0e8b30c97a7a\"}",621);
+  insert_document_into_collection_json(database_name,"delegates","{\"public_address\":\"XCA1u5AWxhjCtRKzZiyUUmdHLnQFQdFcZGtuFxx9JW21hWJnzgf31AV2g58gUsN3aKSju1iL9RdzbHJqvUKjVYBh67e74Zc38v\",\"total_vote_count\":\"100000000\",\"IP_address\":\"192.168.1.220\",\"delegate_name\":\"delegate_name_20\",\"about\":\"\",\"website\":\"\",\"team\":\"\",\"pool_mode\":\"false\",\"fee_structure\":\"\",\"server_settings\":\"\",\"block_verifier_score\":\"0\",\"online_status\":\"true\",\"block_verifier_total_rounds\":\"0\",\"block_verifier_online_total_rounds\":\"0\",\"block_verifier_online_percentage\":\"0\",\"block_producer_total_rounds\":\"0\",\"block_producer_block_heights\":\"\",\"public_key\":\"ad98cadb3b13229c78709876955247cbef40d5b15c4842be605b0e8b30c97a7a\"}",620);
+  insert_document_into_collection_json(database_name,"delegates","{\"public_address\":\"XCA1u5AWxhjCtRKzZiyUUmdHLnQFQdFcZGtuFxx9JW21hWJnzgf31AV2g58gUsN3aKSju1iL9RdzbHJqvUKjVYBh67e74Zc38v\",\"total_vote_count\":\"10000000\",\"IP_address\":\"192.168.1.220\",\"delegate_name\":\"delegate_name_20\",\"about\":\"\",\"website\":\"\",\"team\":\"\",\"pool_mode\":\"false\",\"fee_structure\":\"\",\"server_settings\":\"\",\"block_verifier_score\":\"0\",\"online_status\":\"true\",\"block_verifier_total_rounds\":\"0\",\"block_verifier_online_total_rounds\":\"0\",\"block_verifier_online_percentage\":\"0\",\"block_producer_total_rounds\":\"0\",\"block_producer_block_heights\":\"\",\"public_key\":\"ad98cadb3b13229c78709876955247cbef40d5b15c4842be605b0e8b30c97a7a\"}",619);
+  insert_document_into_collection_json(database_name,"delegates","{\"public_address\":\"XCA1u5AWxhjCtRKzZiyUUmdHLnQFQdFcZGtuFxx9JW21hWJnzgf31AV2g58gUsN3aKSju1iL9RdzbHJqvUKjVYBh67e74Zc38v\",\"total_vote_count\":\"5000000000\",\"IP_address\":\"192.168.1.220\",\"delegate_name\":\"delegate_name_20\",\"about\":\"\",\"website\":\"\",\"team\":\"\",\"pool_mode\":\"false\",\"fee_structure\":\"\",\"server_settings\":\"\",\"block_verifier_score\":\"0\",\"online_status\":\"true\",\"block_verifier_total_rounds\":\"0\",\"block_verifier_online_total_rounds\":\"0\",\"block_verifier_online_percentage\":\"0\",\"block_producer_total_rounds\":\"0\",\"block_producer_block_heights\":\"\",\"public_key\":\"ad98cadb3b13229c78709876955247cbef40d5b15c4842be605b0e8b30c97a7a\"}",621);
+  memset(result_test,0,sizeof(result_test));
+  memcpy(result_test,"{\"public_address_created_reserve_proof\":\"" TEST_WALLET "\",\"public_address_voted_for\":\"",169);
+  memcpy(result_test+strlen(result_test),xcash_wallet_public_address,XCASH_WALLET_LENGTH);
+  memcpy(result_test+strlen(result_test),"\",\"total\":\"2000000000\",\"reserve_proof\":\"DATA\"}",46);
+  insert_document_into_collection_json(database_name,"reserve_proofs_1",result_test,0);
   RESET_ERROR_MESSAGES;
 
   // reset the variables
@@ -71,10 +84,53 @@ int shared_delegate_website_functions_test(void)
   fprintf(stderr,"\033[1;34m%s\nshared_delegate website functions test - Total test: %d\n%s\n\n\033[0m",TEST_OUTLINE,SHARED_DELEGATE_WEBSITE_FUNCTIONS_TOTAL_TEST,TEST_OUTLINE);
  
   // run the test
-  CHECK_WEBSITE_SERVER_FUNCTIONS("server_receive_data_socket_shared_delegates_website_get_statistics",SERVER_RECEIVE_DATA_SOCKET_SHARED_DELEGATES_WEBSITE_GET_STATISTICS_TEST_DATA);
-  CHECK_WEBSITE_SERVER_FUNCTIONS("server_receive_data_socket_get_blocks_found",SERVER_RECEIVE_DATA_SOCKET_GET_BLOCKS_FOUND_TEST_DATA);
-  CHECK_WEBSITE_SERVER_FUNCTIONS("server_receive_data_socket_get_public_address_information",SERVER_RECEIVE_DATA_SOCKET_GET_PUBLIC_ADDRESS_INFORMATION_TEST_DATA);
-  CHECK_WEBSITE_SERVER_FUNCTIONS("server_receive_data_socket_get_public_address_payment_information",SERVER_RECEIVE_DATA_SOCKET_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION_TEST_DATA);
+  memset(data_test,0,sizeof(data_test));
+  if (send_and_receive_data_socket(data_test,"127.0.0.1",SEND_DATA_PORT,SERVER_RECEIVE_DATA_SOCKET_SHARED_DELEGATES_WEBSITE_GET_STATISTICS_TEST_DATA,RECEIVE_DATA_TIMEOUT_SETTINGS,"",0) == 1 && parse_http_response(data_test) == 1 && strstr(data_test,xcash_wallet_public_address) != NULL && strstr(data_test,"\"current_delegate_rank\":\"2\"") != NULL && strstr(data_test,"\"total_votes\":\"2000000000\"") != NULL && strstr(data_test,"\"online_percentage\":\"100\"") != NULL && strstr(data_test,"\"total_blocks_found\":\"2\"") != NULL && strstr(data_test,"\"total_xcash_from_blocks_found\":\"25\"") != NULL && strstr(data_test,"\"total_payments\":\"2\"") != NULL && strstr(data_test,"\"fee\":\"0.000000\"") != NULL && strstr(data_test,"\"minimum_amount\":\"0\"") != NULL)
+  {
+    color_print("PASSED! Test for server_receive_data_socket_shared_delegates_website_get_statistics","green");
+    count_test++;
+  }
+  else
+  {
+    color_print("FAILED! Test for server_receive_data_socket_shared_delegates_website_get_statistics","red");
+  }
+  RESET_ERROR_MESSAGES;
+  
+  memset(data_test,0,sizeof(data_test));
+  if (send_and_receive_data_socket(data_test,"127.0.0.1",SEND_DATA_PORT,SERVER_RECEIVE_DATA_SOCKET_GET_BLOCKS_FOUND_TEST_DATA,RECEIVE_DATA_TIMEOUT_SETTINGS,"",0) == 1 && parse_http_response(data_test) == 1 && strncmp(data_test,GET_BLOCKS_FOUND_TEST_DATA_RESULTS,sizeof(data_test)) == 0)
+  {
+    color_print("PASSED! Test for server_receive_data_socket_get_blocks_found","green");
+    count_test++;
+  }
+  else
+  {
+    color_print("FAILED! Test for server_receive_data_socket_get_blocks_found","red");
+  }
+  RESET_ERROR_MESSAGES;
+
+  memset(data_test,0,sizeof(data_test));
+  if (send_and_receive_data_socket(data_test,"127.0.0.1",SEND_DATA_PORT,SERVER_RECEIVE_DATA_SOCKET_GET_PUBLIC_ADDRESS_INFORMATION_TEST_DATA,RECEIVE_DATA_TIMEOUT_SETTINGS,"",0) == 1 && parse_http_response(data_test) == 1 && strncmp(data_test,GET_PUBLIC_ADDRESS_INFORMATION_DATA_RESULTS,sizeof(data_test)) == 0) 
+  {
+    color_print("PASSED! Test for server_receive_data_socket_get_public_address_information","green");
+    count_test++;
+  }
+  else
+  {
+    color_print("FAILED! Test for server_receive_data_socket_get_public_address_information","red");
+  }
+  RESET_ERROR_MESSAGES;
+
+  memset(data_test,0,sizeof(data_test));
+  if (send_and_receive_data_socket(data_test,"127.0.0.1",SEND_DATA_PORT,SERVER_RECEIVE_DATA_SOCKET_GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION_TEST_DATA,RECEIVE_DATA_TIMEOUT_SETTINGS,"",0) == 1 && parse_http_response(data_test) == 1 && strncmp(data_test,GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION_DATA_RESULTS,sizeof(data_test)) == 0)
+  {
+    color_print("PASSED! Test for server_receive_data_socket_get_public_address_information","green");
+    count_test++;
+  }
+  else
+  {
+    color_print("FAILED! Test for server_receive_data_socket_get_public_address_information","red");
+  }
+  RESET_ERROR_MESSAGES;
 
 
 
@@ -111,4 +167,7 @@ int shared_delegate_website_functions_test(void)
   return count_test;
 
   #undef SHARED_DELEGATE_WEBSITE_FUNCTIONS_TOTAL_TEST
+  #undef GET_BLOCKS_FOUND_TEST_DATA_RESULTS
+  #undef GET_PUBLIC_ADDRESS_INFORMATION_DATA_RESULTS
+  #undef GET_PUBLIC_ADDRESS_PAYMENT_INFORMATION_DATA_RESULTS
 }
