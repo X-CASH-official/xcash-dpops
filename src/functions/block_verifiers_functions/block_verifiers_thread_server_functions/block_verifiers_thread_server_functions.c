704d95dd1bfbe8b577114c5cab0f639d91267d59
zachhildreth <zachariahildreth@gmail.com>
2020-08-08 14:44:26 +0000

    fix: Remove replayed round block
    Remove replayed round block from db if it is there

--- src/functions/block_verifiers_functions/block_verifiers_thread_server_functions/block_verifiers_thread_server_functions.c
+++ src/functions/block_verifiers_functions/block_verifiers_thread_server_functions/block_verifiers_thread_server_functions.c
@@ -140,6 +140,21 @@ void* current_block_height_timer_thread(void* parameters)
       // check if this round is a replayed round
       replayed_round_settings = strncmp(previous_block_height,current_block_height,BUFFER_SIZE) == 0 ? 1 : 0;
 
+      // remove any database data if its a replayed round
+      if (replayed_round_settings == 1)
+      {
+        memset(data,0,sizeof(data));
+        memset(data2,0,sizeof(data2));
+        get_reserve_bytes_database(count,0); 
+        memcpy(data,"reserve_bytes_",14);
+        snprintf(data+14,sizeof(data)-15,"%zu",count);  
+        memcpy(data2,"{\"block_height\":\"",17);
+        memcpy(data2+17,current_block_height,strnlen(current_block_height,sizeof(data2)));
+        memcpy(data2+strlen(data2),"\"}",2);   
+        delete_document_from_collection(database_name,data,data2);
+        RESET_ERROR_MESSAGES;
+      }
+
       // copy the current block height
       memset(previous_block_height,0,sizeof(previous_block_height));
       memcpy(previous_block_height,current_block_height,strnlen(current_block_height,sizeof(previous_block_height)));
