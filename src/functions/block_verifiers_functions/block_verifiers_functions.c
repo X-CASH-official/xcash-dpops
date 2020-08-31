81b41a6ab12fd503795d1adc085c0e2595832476
zachhildreth <zachariahildreth@gmail.com>
2020-08-31 15:46:11 +0000

    fix: dont run online status in registration mode
    Dont run the online status check if running in registration mode.

--- src/functions/block_verifiers_functions/block_verifiers_functions.c
+++ src/functions/block_verifiers_functions/block_verifiers_functions.c
@@ -120,7 +120,10 @@ int start_new_round(void)
   color_print("getting the delegates online status","blue");
 
   // get the delegates online status
-  get_delegates_online_status();
+  if (registration_settings != 1)
+  {
+    get_delegates_online_status();
+  }
 
   // save a copy of the database
   if (production_settings == 1 && network_data_node_settings == 1)
