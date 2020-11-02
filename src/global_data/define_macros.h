a037f0801415f551af505bac649343a730b885b6
zachhildreth <zachariahildreth@gmail.com>
2020-11-02 18:23:27 +0000

    test: internal test 1
    removed majority check at startup since its not needed since a DBFT occurs each round
    
    moved the backup of the DB to after a block is created, this way it does not slow down the seed nodes on the timings.

--- src/global_data/define_macros.h
+++ src/global_data/define_macros.h
@@ -75,7 +75,7 @@ Using define statements instead of constants for increased efficiency
 #define INVALID_BLOCK_VERIFIERS_AMOUNT 10 // The maximum amount of times your node did not receive the block from the main network backup node, to indicate if your node is not syncing
 #define MINIMUM_AMOUNT_RESERVE_PROOF 2000000000000 // The minimum amount to create a reserve proof, 2M xcash
 #define MINIMUM_AMOUNT_REGISTER_DELEGATE 1000000 // The minimum amount to keep a registered delegate from being removed from the inactive delegates timer thread, if the delegate has not mined a block yet, 20M xcash
-#define DATABASE_DATA_SYNC_DELEGATE_MINIMUM_AMOUNT 2000000000000 // The minimum amount to sync the database. This is used for delegates below the top 100 delegates that want to leave the xcash-dpops program running to sync in case they move to the top 100, 100M xcash
+#define DATABASE_DATA_SYNC_DELEGATE_MINIMUM_AMOUNT 0 // The minimum amount to sync the database. This is used for delegates below the top 100 delegates that want to leave the xcash-dpops program running to sync in case they move to the top 100, 100M xcash
 #define START_TIME_MINUTE_BLOCK_VERIFIERS_SYNCHRONIZE_DATABASE 1 // the start time in minutes that the block verifiers synchronize the database each round
 #define START_TIME_SECONDS_BLOCK_VERIFIERS_SYNCHRONIZE_DATABASE 10 // the start time in seconds that the block verifiers synchronize the database each round
 #define SUBMIT_NETWORK_BLOCK_TIME_SECONDS 15 // The time to submit the network block
