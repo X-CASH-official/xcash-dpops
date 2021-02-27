c6469bdc70b8b231f8e44b3690a0687359b166e0
zachhildreth <zachariahildreth@gmail.com>
2021-02-27 09:15:08 -0800

    fix: remove block height check sending hourly payments
    remove block height check from sending hourly payments, not needed once mainnet started

--- src/functions/shared_delegates_website_functions/shared_delegate_website_thread_server_functions/shared_delegate_website_thread_server_functions.c
+++ src/functions/shared_delegates_website_functions/shared_delegate_website_thread_server_functions/shared_delegate_website_thread_server_functions.c
@@ -853,8 +853,7 @@ void* payment_timer_thread(void* parameters)
     start:
     // check if it is time to send the payments
     get_current_UTC_time(current_date_and_time,current_UTC_date_and_time);
-    sscanf(current_block_height, "%lld", &number);
-    if (current_UTC_date_and_time.tm_min == (BLOCK_TIME-1) && number > XCASH_PROOF_OF_STAKE_BLOCK_HEIGHT)
+    if (current_UTC_date_and_time.tm_min == (BLOCK_TIME-1))
     {
       // wait to make sure the checking of reserve proofs have stoped
       sleep(5);
