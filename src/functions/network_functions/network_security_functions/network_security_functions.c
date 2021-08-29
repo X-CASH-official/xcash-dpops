523c42f3b7d6d4369d17df6df2d2b3c25ecd2018
farmage <farmage@protonmail.com>
2021-08-29 11:30:24 +0300

    better leave it alone, there is locig broken atall

--- src/functions/network_functions/network_security_functions/network_security_functions.c
+++ src/functions/network_functions/network_security_functions/network_security_functions.c
@@ -528,12 +528,12 @@ int verify_data(const char* MESSAGE, const int VERIFY_CURRENT_ROUND_PART_AND_CUR
   {
     start:
 
-    // check for non-json message, there is no signature, we have nothing to check
-    if (strstr(MESSAGE,"NODE_TO_BLOCK_VERIFIERS_GET_RESERVE_BYTES_DATABASE_HASH") != NULL &&  strstr(MESSAGE,"}") == NULL){
-          color_print("Special case for non-json NODE_TO_BLOCK_VERIFIERS_GET_RESERVE_BYTES_DATABASE_HASH. Skip signature validation","green");
-          //stupid logic solution because of above "start:" label
-          goto we_re_done_ok;
-    }
+    // // check for non-json message, there is no signature, we have nothing to check
+    // if (strstr(MESSAGE,"NODE_TO_BLOCK_VERIFIERS_GET_RESERVE_BYTES_DATABASE_HASH") != NULL &&  strstr(MESSAGE,"}") == NULL){
+    //       color_print("Special case for non-json NODE_TO_BLOCK_VERIFIERS_GET_RESERVE_BYTES_DATABASE_HASH. Skip signature validation","green");
+    //       //stupid logic solution because of above "start:" label
+    //       goto we_re_done_ok;
+    // }
 
     // get the public key, proof and beta string
     memcpy(proof,XCASH_DPOPS_signature,VRF_PROOF_LENGTH);
