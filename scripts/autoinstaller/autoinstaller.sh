2e7997563c558169a1fb4a39bb015bf0c7d986c5
zachhildreth <zachariahildreth@gmail.com>
2022-05-24 15:37:40 -0700

    fix: Autoinstaller
    autoinstaller fix

--- scripts/autoinstaller/autoinstaller.sh
+++ scripts/autoinstaller/autoinstaller.sh
@@ -924,9 +924,9 @@ function install_mongodb()
   # check if we need to install libssl 1.1
   UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}' | sed s"|\.||g")
   if [ "$UBUNTU_VERSION" -ge 2204 ]; then
-    wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb
-    sudo dpkg -i libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb &>/dev/null
-    rm libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb &>/dev/null
+    wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.3_amd64.deb
+    sudo dpkg -i libssl1.1_1.1.1l-1ubuntu1.3_amd64.deb &>/dev/null
+    rm libssl1.1_1.1.1l-1ubuntu1.3_amd64.deb &>/dev/null
   fi
 
   echo -ne "\r${COLOR_PRINT_GREEN}Installing MongoDB${END_COLOR_PRINT}"
